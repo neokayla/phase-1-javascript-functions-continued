47f4110706838a563ec22626f0f34da68d6c06c5
Kayla Lebron <kayla.lebron12@gmail.com>
2023-06-01 19:48:55 -0400

    completed

--- index.js
+++ index.js
@@ -1,1 +1,11 @@
-// code your solution here
+function saturdayFun (act = "roller-skate") {
+    return `This Saturday, I want to ${act}!`
+}
+function mondayWork (thing = 'go to the office') {
+    return `This Monday, I will ${thing}.`
+} 
+function wrapAdjective (para = "*") {
+    return function inner (para1 = 'special') {
+        return `You are ${para}${para1}${para}!`
+    }
+}
